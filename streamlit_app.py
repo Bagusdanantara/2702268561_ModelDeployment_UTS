7427a1338f34e44aa0cb1f68f378e35dc2dc5c2e
Bagusdanantara <bagus.danantara04@gmail.com>
2025-04-22 08:22:09 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,23 @@ import pandas as pd
 import numpy as np
 import pickle
 
-# Load saved model and preprocessing objects
+# Load saved model
+with open('xgboost_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+# Load saved scaler
+with open('scaler.pkl', 'rb') as f:
+    scaler = pickle.load(f)
+
+# Load saved label encoders
+with open('label_encoders.pkl', 'rb') as f:
+    label_encoders = pickle.load(f)
+
 with open('xgboost_model.pkl', 'rb') as f:
     data = pickle.load(f)
-    model = data['/Users/bagusdanantaras/Downloads/xgb_model_real.pkl']
-    label_encoders = data['/Users/bagusdanantaras/Downloads/label_encoders_real.pkl']
-    scaler = data['/Users/bagusdanantaras/Downloads/scaler_real.pkl']
+    model = data['model']
+    label_encoders = data['label_encoders']
+    scaler = data['scaler']
 
 # Define feature columns
 categorical_columns = ['person_gender', 'person_education', 'loan_intent',
